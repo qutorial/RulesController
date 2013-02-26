c0cae9a40649cace2266b57f21e5381bedb37532
Zaur Molotnikov <qutorial@gmail.com>
2013-02-26 16:33:58 +0100

    Fixing Yices impl from MPS - before Builder mods

---
+++ rulescontroller/src/main/java/com/mbeddr/ext/utils/tools/Pair.java
@@ -0,0 +1,12 @@
+package com.mbeddr.ext.utils.tools;
+
+public class Pair<T1, T2> {
+	private T1 mLeft;
+	private T2 mRight;
+
+	public Pair(T1 left, T2 right) {
+		mLeft = left;
+		mRight = right; 
+	}
+
+}
